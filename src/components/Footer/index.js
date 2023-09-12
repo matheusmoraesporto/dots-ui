05459cfc5318971f12b196b0e298be58bf78623a
Matheus Porto <matheus.porto@sap.com>
2023-09-12 12:02:27 -0300

    Create footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './styles.css';
 
 function Footer() {
     return (
-        <footer>
-            TEST FOOTER
+        <footer className="Footer">
+            <p>Developed by Matheus Porto</p>
+            <a href="https://github.com/matheusmoraesporto/dots-ui" target="_blank">
+                <i className="fa fa-github"></i>
+            </a>
         </footer>
     )
 }
