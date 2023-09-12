da6594e72d65d36cf0522912aaeff6e3868e969d
Matheus Porto <matheus.porto@sap.com>
2023-09-11 23:41:34 -0300

    Create header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import AygaLogo from "../../assets/ayga-logo.png";
+import './styles.css';
 
 function Header() {
     return (
-        <header>Header</header>
+        <header className="Header">
+            <img src={AygaLogo} className="logo"></img>
+            <h3>Bem-vindo usuário</h3>
+        </header>
     );
 }
 
